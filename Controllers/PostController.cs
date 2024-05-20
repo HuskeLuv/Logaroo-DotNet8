75476570784a4e7f0efffb888214aee130d13eaf
Wellington <huske.xp@gmail.com>
2024-05-20 14:47:23 -0300

    chore: Iniciando controller de Posts

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -1,16 +1,29 @@
+using app.Database;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace post.Controllers
 {
-    [ApiController]
     [Route("api/[controller]")]
+    [ApiController]
     public class PostController : ControllerBase
     {
-        [HttpGet, Authorize]
-        public string Get()
+        private readonly DataContext _context;
+        public PostController(DataContext context)
         {
-            return "Ok";
+            _context = context;
         }
+
+        [HttpGet, Authorize]
+        public IActionResult GetAll([FromQuery] string? tag = null)
+        {
+            var posts = _context.Posts.AsQueryable();
+            if (!string.IsNullOrEmpty(tag)){
+                posts = posts.Where(post => post.Tags.Contains(tag));
+            }
+
+            return Ok(posts.ToList());
+        }
+
     }
 }
